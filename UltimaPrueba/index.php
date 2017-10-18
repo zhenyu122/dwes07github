144c2f0dcb4e7cdca4d4388fd58bbe0da4cf29e2
Alumno <Alumno@W7Bsica>
2017-10-18 10:28:59 +0200

    s

--- UltimaPrueba/index.php
+++ UltimaPrueba/index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Primer ejemplo de PHP</title>
+</head>
+<body>
 <?php 
 echo "<h1>Funciona</h1>";
+$a="Zhen";
+echo "<p>Prueba de $a </p>";
+echo 'Prueba $a';
+
+define("ALUMNO","Zhen");
+echo "<p>Prueba de ".ALUMNO ." </p>";
 ?>
+</body>
+</html>
