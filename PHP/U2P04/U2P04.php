579cb9a8bcc3e47a0456f7e66d330814c0b36804
Alumno <Alumno@W7Bsica>
2017-10-30 10:45:43 +0100

    a

---
+++ PHP/U2P04/U2P04.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title></title>
+</head>
+<body>
+<h1>Diferencia</h1>
+<?php 
+if (!isset($_POST["enviar"])){
+?>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"],ENT_QUOTES,"UTF-8")?>" method="post">
+Numero A<input type="text" name="a">
+Numero B<input type="text" name="b">
+<input type="submit" name=enviar>
+</form>
+<?php 
+}else{
+    
+}
+?>
+<br>
+<a href="index.php"><input type="button" value="Volver"></a>
+</body>
+</html>
