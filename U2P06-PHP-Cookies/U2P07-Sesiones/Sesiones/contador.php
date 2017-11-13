1e39eb90014c08244e24a5db4a1755a805c3782e
Alumno <Alumno@W7Bsica>
2017-11-13 12:43:34 +0100

    2

---
+++ U2P06-PHP-Cookies/U2P07-Sesiones/Sesiones/contador.php
@@ -0,0 +1,55 @@
+<?php
+if (session_status () == PHP_SESSION_NONE){
+    session_name('idSesion07');
+    session_start ();
+}
+if (isset($_REQUEST["reiniciarContador"])) {
+    unset($_SESSION["contador"]);
+}
+if (isset($_REQUEST["cerrarSesion"])) {
+    $_SESSION=array();
+    session_unset();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+            );
+    }
+    session_destroy();
+}
+
+if(session_status() == PHP_SESSION_NONE)
+    $mensaje = "No hay sesión iniciada";
+    else {
+    if (isset ( $_SESSION ['contador'] ))
+            $_SESSION ['contador'] += 1;
+            else
+                $_SESSION ['contador'] = 1;
+                if(isset($_POST["enviar"]))
+                    $_SESSION ['contador']+=$_POST["valor"];
+                $mensaje = "Has visitado esta página " . $_SESSION ['contador'] . " veces en esta sesión.";
+}
+?>
+<html>
+<head>
+<title>Sesiones</title>
+<meta charset="UTF-8"/>
+</head>
+<body>
+<h3><?php echo $mensaje;?></h3>
+<?php 
+if (!isset($_POST["enviar"])){
+?>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"],ENT_QUOTES,"UTF-8")?>" method="post">
+Vsitas<input type="text" name="valor">
+<input type="submit" name="enviar">
+</form>
+<?php 
+}
+?>
+<p><a href="<?php echo $_SERVER['PHP_SELF']?>">Recargar la página</a></p>
+<p><a href="<?php echo $_SERVER['PHP_SELF']."?reiniciarContador=true"?>">Reiniciar contador</a></p>
+<p><a href="<?php echo $_SERVER['PHP_SELF']."?cerrarSesion=true"?>">Cerrar sesión</a></p>
+
+</body></html>
