db49b6a905b6d77c94bcfa1744e823be5b593c3c
Alumno <Alumno@W7Bsica>
2017-11-22 11:49:43 +0100

    a

---
+++ U3P02-PHP-BBDD/BBDD.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Conexión a BBDD con PHP</title>
+	<meta charset="UTF-8"/>
+</head>
+<body>
+<h2>Pruebas con la base de datos de animales</h2>
+<?php
+$servidor = "localhost";
+$usuario = "alumno";
+$clave = "alumno";
+?>
+<p> Vamos a utilizar las siguientes variables:</p>
+<ul>
+<?php
+echo "<li>Nombre del servidor al que nos vamos a conectar a MySQL: $servidor</li>";
+echo "<li>Nombre de usuario con el que nos vamos a conectar a MySQL: $usuario</li>";
+echo "<li>Contraseña del usuario en MySQL: $clave</li>";
+?>
+</ul>
+</body>
+</html>
