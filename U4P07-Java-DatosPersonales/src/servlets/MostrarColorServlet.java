85d6f13d3c8d0f3bc1241f04d6bc48477bbb0a10
Alumno <Alumno@W7Bsica>
2018-02-07 11:04:03 +0100

    s

--- U4P07-Java-DatosPersonales/src/servlets/MostrarColorServlet.java
+++ U4P07-Java-DatosPersonales/src/servlets/MostrarColorServlet.java
@@ -35,13 +35,13 @@ public class MostrarColorServlet extends HttpServlet {
 			if(request.getParameter("color")==null || request.getParameter("nombre")==null) {
 				response.sendRedirect("./colores.html");
 				}
-			
+			out.println("<html><head></head><body>");
 			String color=request.getParameter("color");
 			int numero = (int)request.getAttribute("numero");
 			String nombre= request.getParameter("nombre");
 				out.println("<h3>"+nombre+"</h3>");
 				out.println("<h2 style='color:"+color+";'>"+numero+"</h2>");
-				
+				out.println("</body></html>");
 			
 			out.close();
 		}
